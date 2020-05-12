b7ce362662faf63e96b4f22bff3f22ca016c398f
en2 <ernest.szczepaniak.en2@gmail.com>
2020-05-12 19:38:20 +0200

    cmake

--- cmake/CMakeConfigBasic.cmake
+++ cmake/CMakeConfigBasic.cmake
@@ -39,8 +39,11 @@ set(board                       de10standard)
 
 set(sdram_start                 0x00000000)
 set(sdram_size                  1024M)
+set(sdram_size_stack            2M)
+set(sdram_size_pool             512M)
+
 set(ocram_start                 0xffff0000)
 set(ocram_size                  64K)
-set(stack_size                  128K)
-set(pool_size                   900M)
+set(ocram_size_stack            4K)
+set(ocram_size_pool             48K)
 
